0895a33c6c7a9dbce2841a7c428263211a97a83e
Aditya Hansda <adityahansda10@gmail.com>
2023-10-22 22:47:08 +0530

    New update
    1. fix bugs
    2.Menu add
    3. Hover features added

--- Script/popup.js
+++ Script/popup.js
@@ -1,3 +1,4 @@
+/*
 document.addEventListener("DOMContentLoaded", function() {
     const popup = document.getElementById("popup");
 
@@ -8,4 +9,23 @@ document.addEventListener("DOMContentLoaded", function() {
     setTimeout(function() {
         popup.classList.remove("show");
     }, 3000);
+});
+
+*/
+
+
+// Get the input element and search icon element by their IDs
+const search_inp = document.getElementById("search_inp");
+const search_icon = document.getElementById("search_icon");
+
+// Add an event listener to the input element for focus event
+search_inp.addEventListener("focus", function() {
+    // Hide the search icon when input is in focus
+    search_icon.style.display = "none";
+});
+
+// Add an event listener to the input element for blur event
+search_inp.addEventListener("blur", function() {
+    // Show the search icon when input is not in focus
+    search_icon.style.display = "inline-block"; // or "block", depending on your layout
 });
